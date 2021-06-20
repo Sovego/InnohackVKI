fe6d757b853f11408cea3815b43f9ff84eb95284
Дмитрий <78340117+enyaaad@users.noreply.github.com>
2021-06-20 17:11:03 +0700

    result

---
+++ Data/NETRUSHContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NETRUSH.Models;
+
+namespace NETRUSH.Data
+{
+    public class NETRUSHContext : DbContext
+    {
+        public NETRUSHContext (DbContextOptions<NETRUSHContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NETRUSH.Models.quest> Questions { get; set; }
+    }
+}
